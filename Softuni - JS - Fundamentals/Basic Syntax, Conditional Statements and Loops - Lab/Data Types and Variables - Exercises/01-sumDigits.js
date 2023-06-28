75e872b1506e54dfbf2edb737f371b3964444fbd
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-06-28 15:44:34 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Data Types and Variables - Exercises/01-sumDigits.js
@@ -0,0 +1,13 @@
+function sumDigits(num) {
+    let sum = 0;
+    let numAsString = String(num);
+
+    for (let i = 0; i < numAsString.length; i++) {
+        sum += Number(numAsString[i]);
+    }
+
+    console.log(sum);
+
+}
+
+sumDigits(245678);
