b14325b8b622f09a135fb01d61a033758f4cb910
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-07-13 12:40:39 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Data Types and Variables - Exercises/07.-lowerRrUpper.js
@@ -0,0 +1,13 @@
+function solve(char) {
+
+    console.log(char === char.toUpperCase() ? 'upper-case' : 'lower-case');
+
+    // if (char === char.toUpperCase()) {
+    //     console.log(`upper-case`);
+    // } else {
+    //     console.log(`lower-case`);
+    // }
+
+}
+
+solve('L');
