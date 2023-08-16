8183caf3bed3a41414749ef88cd03df8a7d28f4f
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-08-16 15:02:46 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Arrays Advanced - Lab/06. Smallest Two Numbers.js
@@ -0,0 +1,8 @@
+function smallestOfTwo(nums) {
+
+    // return nums.sort((a, b) => a - b).slice(0, 2).join(' ');
+    
+}
+nums => nums.sort((a, b) => a - b).slice(0, 2).join(' ');
+
+console.log(smallestOfTwo([30, 15, 50, 5]));
