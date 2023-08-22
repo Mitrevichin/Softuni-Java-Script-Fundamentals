fcbe0cbffa945d31b9bf3771b2c542306fb1a633
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-08-22 12:35:42 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Arrays Advanced - Exercises/04-Sorting.js
@@ -0,0 +1,14 @@
+function sorting(array) {
+    let newArr = [];
+
+    array.sort((a, b) => b - a);
+    while (array.length > 0) {
+        let biggerNum = array.shift();
+        newArr.push(biggerNum);
+        let smallerNum = array.pop();
+        newArr.push(smallerNum);
+    }
+    console.log(newArr.join(' '));
+}
+
+sorting([1, 21, 3, 52, 69, 63, 31,2, 18, 94]);
