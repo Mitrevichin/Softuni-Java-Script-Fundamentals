7aa27c3bc40af769c80c0da382ca1313b4a41dee
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-08-27 11:31:32 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Objects and Classes - Lab/01-personalInfo.js
@@ -0,0 +1,10 @@
+function personalInfo(firstName, lastName, age) {
+    let result = {
+        firstName : firstName,
+        lastName : lastName,
+        age : age
+    }
+    return result;
+}
+
+personalInfo();
