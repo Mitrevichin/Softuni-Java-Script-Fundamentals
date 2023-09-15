76519376ef891e8a3ea2eff08ebcf399931ba603
Petar Mitrevichin <pmitrevichin@gmail.com>
2023-09-15 10:43:12 +0300

    Add files via upload

---
+++ Softuni - JS - Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/Text Processing - Lab/01. Print Characters.js
@@ -0,0 +1,12 @@
+function solve(str) {
+    // for (let char of str) {
+    //     console.log(char);
+    // }
+   
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i]);
+    }
+
+}
+
+solve('AWord');
